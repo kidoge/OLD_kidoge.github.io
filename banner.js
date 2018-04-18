ece316489bf0603b190b1ed802a299007b014dc1
Kidon Son <kidonson@gmail.com>
2018-04-18 09:25:22 -0400

    Use simple faded canvas animation

--- banner.js
+++ banner.js
@@ -1,5 +1,62 @@
+"use strict";
+
 var canvas = null;
 var ctx = null;
+
+var lastTime = null;
+
+var x = null;
+var y = null;
+var type = null; // 0 = black, 1 = white
+
+var VX = 0.4;
+var VY = 0.8;
+
+function resetLine() {
+  x = Math.random() * (canvas.width + canvas.height / 2) - canvas.height / 2;
+  y = 0;
+  type = Math.floor(Math.random() * 2);
+}
+function init() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.lineWidth = 2;
+  resetLine();
+
+
+  timeElapsed();
+  requestAnimationFrame(refresh);
+}
+
+function timeElapsed() {
+  var now = Date.now();
+  var timeDelta = now - lastTime;
+  lastTime = now;
+  return timeDelta;
+}
+function refresh() {
+  var timeDelta = timeElapsed();
+  if (y >= canvas.height) {
+    resetLine();
+  }
+
+  var newY = y + timeDelta * VY;
+  if (type === 0) {
+    var newX = x + timeDelta * VX;
+    ctx.strokeStyle = "black";
+  } else {
+    var newX = x - timeDelta * VX;
+    ctx.strokeStyle = "white";
+  }
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(newX, newY);
+  ctx.closePath();
+  ctx.stroke();
+  x = newX;
+  y = newY;
+  requestAnimationFrame(refresh);
+}
 
 function setup() {
   canvas = $('canvas')[0];
@@ -7,13 +64,7 @@ function setup() {
     canvas.width = canvas.scrollWidth;
     canvas.height = canvas.scrollHeight;
     ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.font = 'bold 120px courier';
-    ctx.fillStyle = 'gray';
-    ctx.fillText('Kidon Son', 10, 380);
-    ctx.fillStyle = 'white';
-    ctx.fillText('Kidon Son', 15, 380);
+    init();
   } else {
     alert('Something went wrong.');
   }
